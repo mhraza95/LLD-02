ee5327d65df8d4dffc20b189fde434e1dfab7049
Hassan Raza <mhraza95@gmail.com>
2024-01-09 22:47:58 +0530

    Factory Method Init Commit

---
+++ src/DesignPattern/FactoryMethod/Buttons/Button.java
@@ -0,0 +1,6 @@
+package DesignPattern.FactoryMethod.Buttons;
+
+public interface Button {
+    void render();
+    void onClick();
+}
---
+++ src/DesignPattern/FactoryMethod/Buttons/HtmlButton.java
@@ -0,0 +1,15 @@
+package DesignPattern.FactoryMethod.Buttons;
+
+public class HtmlButton implements Button{
+
+    @Override
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    @Override
+    public void onClick() {
+        System.out.println("Click! Button Says - 'Hello World!'");
+    }
+}
